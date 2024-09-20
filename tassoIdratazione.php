797d3affbb9ffafd5eb38035129e671905add451
AlessioCerame06 <alessiocerame@gmail.com>
2024-09-20 08:31:34 +0200

    Es_1

---
+++ tassoIdratazione.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tasso Idratazione</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+    echo "<h1>Cerame Alessio</h1>";
+    $peso = 60;
+    $tassoIdratazione = 60 * 30;
+    echo "<p>Il tasso di idratazione di una persona che pesa $peso kg è di $tassoIdratazione</p>"
+?>
